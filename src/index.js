ed6970a42ac0a2088456450308e06ecaf32c470c
KgomotsoSalman <164500988+KgomotsoSalman@users.noreply.github.com>
2025-08-01 13:34:48 +0200

    added some attributes and targeted them on javascript and change some few styles on css

--- src/index.js
+++ src/index.js
@@ -1,15 +1,46 @@
+function displayPoem(response) {
+    console.log("poem generated");
+    new Typewriter("#typewriter", {
+        strings: response.data.answer,
+        autoStart: true,
+        delay: 1,
+        cursor: "",
+    });
+}
+
+
+
+
 function generatePoem(event) {
     event.preventDefault();
+
+    let instructionsInput = document.querySelector("#user-instructions")
+
+
+    let apiKey = "dce9442t2bd303co9f5305c7f083f189a";
+    let prompt = 'User instructions are:Generate a Setswana poem about ${instructionsInput.value}';
+    let context = "You are a Setswana poem expert and love to write short poems. Your mission is to generate a 4 line poem in basic HTML and separate each line with a <br/>. Make sure to follow the user instructions.";
+    let apiUrl = 'https://api.shecodes.io/v1/generate?prompt=${prompt}&context=${context}&key=${key}';
+
+
+
+    let poemElement = document.querySelector("#poem");
+    poemElement.classList.remove("hidden");
+    poemElement.innerHTML = 'Generating a Setswana poem about ${instructionsInput.value}';
+
+
+
+    console.log("Generating poem");
+    console.log('Prompt: ${prompt}');
+    console.log('Context: ${context}');
+
+    axios.get(apiUrl).then(displayPoem);
+
 
 
     let poemElement = document.querySelector("#poem");
 
-    new Typewriter("#typewriter", {
-        strings: "poem poem poem",
-        autoStart: true,
-        delay: 1,
-        cursor: ""
-    });
+
 
 }
 
